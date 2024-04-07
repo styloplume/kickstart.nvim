461598d6c2fc0dedb404ec1d94dc0fea606b993b
styloplume <14195928+styloplume@users.noreply.github.com>
2024-04-07 19:25:33 +0200

    cmake-tools etc.

--- tst/tst.cpp
+++ tst/tst.cpp
@@ -1,9 +1,9 @@
 #include <array>
+#include <cstdio>
 #include <iostream>
 #include <vector>
 
-int
-main ()
+int main()
 {
   std::cout << "Hello there\n";
   std::array<int, 3> array;
@@ -12,4 +12,18 @@ main ()
 
   int i = 1;
   return 0;
+}
+
+void test(int i) { printf("%d\n", i); }
+void test(double d)
+{
+  printf("%f\n", d);
+  std::cout << "ok" << std::endl;
+}
+
+void test(float f)
+{
+  int a = 0;
+  std::cout << "hey\n";
+  // why is : removing indentation ?
 }
