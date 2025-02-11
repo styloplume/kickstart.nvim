aef947a53abc46dfaec2fe4324a9db628f193253
styloplume <14195928+styloplume@users.noreply.github.com>
2025-02-11 22:38:25 +0100

    -obsidian

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,40 +3,44 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'epwalsh/obsidian.nvim',
-    version = '*', -- recommended, use latest release instead of latest commit
-    lazy = false,
-    ft = 'markdown',
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
-    dependencies = {
-      -- Required.
-      'nvim-lua/plenary.nvim',
-
-      -- see below for full list of optional dependencies 👇
-    },
-    opts = {
-      workspaces = {
-        -- {
-        --   name = 'personal',
-        --   path = '~/vaults/personal',
-        -- },
-        {
-          name = 'work',
-          path = 'e:/users/wmn/obsidian/wmn',
-        },
-      },
-
-      -- see below for full list of options 👇
-    },
-  },
+  -- {
+  --   'epwalsh/obsidian.nvim',
+  --   version = '*', -- recommended, use latest release instead of latest commit
+  --   lazy = false,
+  --   ft = 'markdown',
+  --   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  --   -- event = {
+  --   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   --   -- refer to `:h file-pattern` for more examples
+  --   --   "BufReadPre path/to/my-vault/*.md",
+  --   --   "BufNewFile path/to/my-vault/*.md",
+  --   -- },
+  --   dependencies = {
+  --     -- Required.
+  --     'nvim-lua/plenary.nvim',
+  --
+  --     -- see below for full list of optional dependencies 👇
+  --   },
+  --   opts = {
+  --     workspaces = {
+  --       -- {
+  --       --   name = 'personal',
+  --       --   path = '~/vaults/personal',
+  --       -- },
+  --       {
+  --         name = 'work',
+  --         path = 'e:/users/wmn/obsidian/wmn',
+  --       },
+  --       {
+  --         name = 'home',
+  --         path = 'd:/Documents/obsidian',
+  --       },
+  --     },
+  --
+  --     -- see below for full list of options 👇
+  --   },
+  -- },
   {
     'lervag/vimtex',
     lazy = false, -- we don't want to lazy load VimTeX
