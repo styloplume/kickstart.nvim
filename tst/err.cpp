0bc3989cf77104bfb4354a932a93a1a22d9a488a
styloplume <14195928+styloplume@users.noreply.github.com>
2025-02-20 21:57:31 +0100

    clang

--- tst/err.cpp
+++ tst/err.cpp
@@ -1,21 +1,41 @@
+// This is a testbed c++ source file to figure out a good set of rules for
+// clang-format, the super cool auto-formatter from LLVM.
+
+// includes are automatically sorted
 #include <array>
 #include <iostream>
 
-template <int i>
-void
-a ()
+// Definition blocks are separated
+class Example
 {
-  a<i + 1> ();
-}
+public:
+  Example();
+  ~Example();
 
-void
-foo ()
-{
-  a<0> ();
-}
+public:
+  void Method();          // so
+  void Method(int);       // this
+  void Method(void *);    // is
+  // a
+private:       // thing
+  int _a;      // ?
+  void *_p;    // kinda cool
+};
 
-int
-main ()
+// Assignments are aligned
+int z    = 0;
+float x  = -2.;
+double c = 300;
+void *v  = NULL;
+// But not through empty lines or comments
+bool ultimate_test = false;
+char b[15];
+
+template <int i> void a() { a<i + 1>(); }
+
+void foo() { a<0>(); }
+
+int main()
 {
   std::cout << "Ok\n";
   std::cout << "Hello clangd\n";
@@ -24,5 +44,24 @@ main ()
   char a[10];
   a[10] = 0;
 
+  // Single line ifs are allowed
+  if (1) std::cout << "Hurra!\n";
+
+  if (0)    // however
+  {
+    std::cout << "then\n";
+    int var = 10;
+  }
+
+  void *ptr = NULL;
+
+  for (int idx = 0; idx <= 10; idx++)
+  {
+    std::cout << idx << std::endl;
+  }
+
   return 0;
 }
+
+// So here we are
+// playing with comments.
